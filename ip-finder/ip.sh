f5ac5fbf8b8e417ff18ea2fce310c0937693fc21
Donald Tran <yosoup@outlook.com>
2017-09-08 16:49:22 -0700

    Update ip.sh
    fix bug where file did not exist before redirecting output to it; changed file names for logs

--- ip-finder/ip.sh
+++ ip-finder/ip.sh
@@ -2,8 +2,9 @@
  
 #  sleep 30
 # done
-
+touch ip_log.txt
+touch dweet_log.txt
 IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 dweet_id_ip="elegant-songs"
-wget --post-data="IP=${IP}" http://dweet.io/dweet/for/${dweet_id_ip} -O /home/pi/Git/ip-finder/log2.txt
-echo "${IP}" >> /home/pi/Git/ip-finder/log.txt
+wget --post-data="IP=${IP}" http://dweet.io/dweet/for/${dweet_id_ip} -O dweet_log.txt
+echo "${IP}" >> ip_log.txt
