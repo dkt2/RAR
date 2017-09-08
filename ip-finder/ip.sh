4c3e878e90c528de1a7252ea90459134084e6a1b
Donald Tran <yosoup@outlook.com>
2017-09-08 16:51:29 -0700

    Revert "add date to ip log"
    This reverts commit be6492cdebce8f1efca42a5a232849278192ef0d.

--- ip-finder/ip.sh
+++ ip-finder/ip.sh
@@ -7,4 +7,4 @@ touch dweet_log.txt
 IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 dweet_id_ip="elegant-songs"
 wget --post-data="IP=${IP}" http://dweet.io/dweet/for/${dweet_id_ip} -O dweet_log.txt
-echo "date | ${IP}" >> ip_log.txt
+echo "${IP}" >> ip_log.txt
