97ec34ee5f1be6e8be166892b7b433a0a268bd93
Donald Tran <yosoup@outlook.com>
2017-09-14 14:21:09 -0700

    reorganize files and folder structure
    and cosmetic changes to battery testing script

--- battery-tester/battery_test.py
+++ battery-tester/battery_test.py
@@ -7,22 +7,23 @@ def get_curr_time():
     return time.asctime(time.localtime(time.time()))
 
 def record_time():
-    with open('../time/time.txt', 'a') as input_file:
+    with open('./time.txt', 'a') as input_file:
         input_file.write(get_curr_time() + '\n')
 
 def clear_log():
-    with open('../time/time.txt', 'w') as input_file:
+    with open('./time.txt', 'w') as input_file:
         input_file.write('')
 
 def main():
     """ main routine """
+    secs = 10
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     clear_log()
+    print("*** Timer Starts")
     while True:
-        print("start timer")
+        print(get_curr_time())
         record_time()
-        time.sleep(10) # wait 60 secs / 1 min
-        print("stop timer")
+        time.sleep(secs) # wait 60 secs / 1 min
 
 if __name__ == '__main__':
     main()
